b30d4fb7da5206c80ac20106ee45f289fd84b5ba
ayush <ayushshaw016@gmail.com>
2024-06-11 17:14:15 +0530

    Storing the Blogs in the database and rendering it on home page

--- Routes/blog_router.js
+++ Routes/blog_router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const blogrouter = express.Router();
 const multer = require("multer");
 const path = require("path");
+const { blogSchema } = require("../Models/BlogSchema");
 blogrouter.get("/addblog", (req, res) => {
   res.render("addblogs", {
     user: req.user,
@@ -18,9 +19,15 @@ const diskstorage = multer.diskStorage({
   },
 });
 const upload = multer({ storage: diskstorage });
-blogrouter.post("/addblog", upload.single("coverimg"), (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-  return res.redirect("/");
+blogrouter.post("/addblog", upload.single("coverimg"), async (req, res) => {
+  // console.log(req.body);
+  // console.log(req.file);
+  const result = await blogSchema.create({
+    title: req.body.title,
+    content: req.body.content,
+    coverimg: `/Blogimg/${req.file.filename}`,
+    createdby: req.user._id,
+  });
+  return res.redirect(`/blog/${result._id}`);
 });
 module.exports = { blogrouter };
--- index.js
+++ index.js
@@ -8,19 +8,25 @@ const {
   checkForAuthenticationCookie,
 } = require("./Middleware/Middleware_Auth");
 const { blogrouter } = require("./Routes/blog_router");
+
+const { blogSchema } = require("./Models/BlogSchema");
+
 // SETTING THE VIEW ENGINE AS ejs
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.use(express.static(path.resolve("./public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieparser());
 app.use(checkForAuthenticationCookie("token"));
 const port = 8000;
 app.listen(port, () => console.log("App is running on port", port));
 dbconnection;
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const blogsss = await blogSchema.find({});
   return res.render("home", {
     user: req.user,
+    blogs: blogsss,
   });
 });
 app.use("/blog", blogrouter);
